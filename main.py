ab6f4adcb5149eccef317ad7198cf108b823c2dc
Umang Srivastava <umang.srivastava.777@gmail.com>
2021-03-16 20:14:57 +0530

    update

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import numpy as np
 
 SECRET_KEY = 'F7U8r4Y2YN0EljgonlgClHUjOIQkHpbnwCcCRi2pTo2GK1m4EZ'
 
-overfit_vector =  [9.913363285632425e-19, -1.3660044023098439e-12, -2.2976634673294506e-13, 5.011345743821108e-11, -1.9164245982279888e-10, -5.706609030574798e-16, 9.211488698839471e-16, 3.215026512103773e-05, -2.106822476052596e-06, -1.3806700813570227e-08, 8.639870129205277e-10]
 
+file = open("generation_03.txt", "w")
+
+overfit_vector = [9.917369362870062e-19, -1.3663345774890496e-12, -2.2987760470899746e-13, 5.0166628893336836e-11, -1.9137079609875392e-10, -5.707209963841367e-16, 9.219501151538645e-16, 3.2090844450778915e-05, -2.103950873147733e-06, -1.3825652924351045e-08, 8.643552922860863e-10]
+# gen1 and gen2
+
+overfit_vector = [9.920228219160712e-19, -1.364202711273918e-12, -2.3006364489300257e-13, 5.0111927990795596e-11, -1.9140452210688034e-10, -5.702463536373112e-16, 9.243845522203362e-16, 3.199653795367389e-05, -2.0985817255189582e-06, -1.3853093822021742e-08, 8.635160890129394e-10]
 
 FITNESS_FACTOR = 2
 VECTOR_SIZE = 11
-POPULATION_COUNT = 6
-GENERATION_COUNT = 8
-MUTATION_PROBABILITY = 0.9
+POPULATION_COUNT = 8
+GENERATION_COUNT = 10
+MUTATION_PROBABILITY = 0.8
 GENOME_LOWER_LIMIT = -10
 GENOME_UPPER_LIMIT = 10
 
@@ -38,12 +43,12 @@ class GA:
         temp =  self.mutation(temp)
         MUTATION_FLAG = 1
         temp[0] = val
-        print("\n")
-        print("\n")
-        print("POPULATION: ")
-        print(temp)
-        print("\n")
-        print("\n")
+        file.write(str("\n"))
+        file.write(str("\n"))
+        file.write(str("POPULATION: "))
+        file.write(str(str(temp)))
+        file.write(str("\n"))
+        file.write(str("\n"))
 
         return temp
 
@@ -58,10 +63,13 @@ class GA:
         validation_errors = []
         feature_fitness = []
         for vector in self.population:
-            print(vector)
+            file.write("\nsending vector for error: ")
+            file.write(str(vector))
             train_error, validation_error = client.get_errors(SECRET_KEY, list(vector))
-            print(train_error,validation_error)
-            fit = -(train_error * FITNESS_FACTOR + validation_error)
+            file.write("\nreceived errors: ")
+            outstr = str(train_error) + "    " + str(validation_error) + "\n"
+            file.write(outstr)
+            fit = -(train_error * FITNESS_FACTOR + validation_error )
             feature_fitness.append((vector,fit))
             fitness.append(fit)
             train_errors.append(train_error)
@@ -71,43 +79,43 @@ class GA:
         self.vector_fitness = feature_fitness
         return fitness, train_errors, validation_errors
 
-    def simple_cross(self, parent1: np.ndarray, parent2:np.ndarray):
-        cutoff = np.random.randint(11)
-        child = parent2
-        child[0:cutoff] = parent1[0:cutoff]
-        return child
+    # def simple_cross(self, parent1: np.ndarray, parent2:np.ndarray):
+    #     cutoff = np.random.randint(11)
+    #     child = parent2
+    #     child[0:cutoff] = parent1[0:cutoff]
+    #     return child
 
     def crossover(self, parent1, parent2):
+        file.write("\n\nCROSSOVER:\n\n")
 
-        print("parent1 = \n")
-        print(parent1)
-        print("parent2 = \n")
-        print(parent2)
+        file.write(str("\n\tparent1 = " + str(parent1)))
+        # file.write(str(parent1)
+        file.write(str("\n\tparent2 = " + str(parent2)))
+        # file.write(str(parent2)
         cutoff = np.random.randint(VECTOR_SIZE)
-        print("cutoff index = ", str(cutoff))
+        file.write(str("\n\tcutoff index = "+ str(cutoff)))
         c1 = parent2
         c1[0:cutoff] = parent1[0:cutoff]
         c2 = parent1
         c2[0:cutoff] = parent2[0:cutoff]
-        print("child 1 = \n")
-        print(c1)
-        print("child 2 = \n")
-        print(c2)
+        file.write(str("\n\tchild 1 = "+ str(c1)))
+        # file.write(str(c1)
+        file.write(str("\n\tchild 2 = "+ str(c2)))
+        file.write(str("\n"))
         return c1,c2
     
     
 
     def mutation(self, feature: np.ndarray):
-        print("\n")
-        print("\n")
-        print("RECEIVED FOR MUTATTION\n")
-        print(feature)
-        print("\n")
-        print("\n")
-
+        file.write(str("\n\nMUTATION: "))
+        file.write(str("\n"))
+        file.write(str("\n"))
+        file.write(str("RECEIVED FOR MUTATTION: "))
+        file.write(str(str(feature)))
+        file.write(str("\n"))
         for idx, val in np.ndenumerate(feature):
             if np.random.random() < MUTATION_PROBABILITY:
-                range_lim = val/2000
+                range_lim = val/1000
                 # popabs = abs(population[idx])
                 noise = np.random.uniform(low=-range_lim, high=range_lim)
                 feature[idx] = feature[idx] + noise
@@ -115,12 +123,10 @@ class GA:
                     feature[idx] = 10.0
                 elif feature[idx] < -10.0:
                     feature[idx] = -10
-        print("\n")
-        print("\n")
-        print("Mutated feature: \n")
-        print(feature)
-        print("\n")
-        print("\n")
+        file.write(str("\nMutated feature:"))
+        file.write(str(str(feature)))
+        file.write(str("\n\n"))
+        # file.write(str("\n")
 
         return feature
 
@@ -189,8 +195,10 @@ MAX_FIT_FLAG = 0
 ga = GA(overfit_vector)
 MUTATION_FLAG = 0
 best_fit_vector, final_fitness, train_error, validation_error = ga.run_GA()
-# print('fitness:')
-# print(final_fitness,train_error,validation_error)
-print('best fit vector:\n', list(best_fit_vector))
-print("{:e}".format(ga.TOT_ERR), "{:e}".format(ga.train_error_for_best_fit_vector), "{:e}".format(ga.validation_error_for_best_fit_vector))
+# file.write(str('fitness:')
+# file.write(str(final_fitness,train_error,validation_error)
+file.write(str('best fit vector:\n'+ str(list(best_fit_vector))))
+# file.write(str("error:")
+file.write(("\nerror:" +"    "+ str("{:e}".format(ga.train_error_for_best_fit_vector)) +"    "+ str("{:e}".format(ga.validation_error_for_best_fit_vector))))
 
+file.close()
